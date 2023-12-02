d68f63e8686b4be3a902c3d34d67119bdc5d07cd
jack <jackayron5@gmail.com>
2023-12-02 12:29:21 +0530

    done

--- index.js
+++ index.js
@@ -1,61 +1,54 @@
-// Import required modules
-const fs = require("fs");
+
+const fs = require("fs").promises;
 const process = require("process");
-const request = require("request");
+const fetch = require("node-fetch");
 
-// Function to make API request and get jokes based on the search term
-function getJokes(searchTerm, callback) {
-  const apiUrl = `https://icanhazdadjoke.com/search?term=${searchTerm}`;
-
-  request(
-    { url: apiUrl, headers: { Accept: "application/json" } },
-    (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        const jokes = JSON.parse(body).results;
-        callback(null, jokes);
-      } else {
-        callback(error || "Unable to fetch jokes from the API");
-      }
-    }
-  );
-}
-
-// Function to select a random joke from the array
-function getRandomJoke(jokes) {
-  return jokes[Math.floor(Math.random() * jokes.length)];
-}
-
-// Function to save joke to a file
-function saveJokeToFile(joke) {
-  fs.appendFile("jokes.txt", `${joke.joke}\n\n`, (err) => {
-    if (err) throw err;
-    console.log("Joke saved to jokes.txt for future laughs!");
-  });
-}
-
-// Command line arguments
 const command = process.argv[2];
 const searchTerm = process.argv[3];
 
 
- if (command === "searchTerm") {
-  if (!searchTerm) {
-    console.error("Please provide a search term for jokes.");
-  } else {
-    getJokes(searchTerm, (error, jokes) => {
-      if (error) {
-        console.error(error);
-      } else if (jokes.length === 0) {
-        console.log(
-          "Sorry, no jokes found for the given search term. The joke gods are taking a day off!"
-        );
-      } else {
-        const randomJoke = getRandomJoke(jokes);
-        console.log(`Here's a joke for you:\n${randomJoke.joke}`);
-        saveJokeToFile(randomJoke);
-      }
+async function getJokes(searchTerm) {
+  const apiUrl = `https://icanhazdadjoke.com/search?term=${searchTerm}`;
+  try {
+    const response = await fetch(apiUrl, {
+      headers: { Accept: "application/json" },
     });
+    const data = await response.json();
+
+    if (data.results && data.results.length > 0) {
+      const randomJoke = getRandomJoke(data.results);
+      console.log(`Here's a joke for you:\n${randomJoke.joke}`);
+      await saveJokeToFile(randomJoke);
+    } else {
+      console.log(
+        "The joke gods are taking a day off!"
+      );
+    }
+  } catch (error) {
+    console.error("Error fetching jokes from the API:", error.message);
   }
+}
+
+// Function to select a random joke 
+function getRandomJoke(jokes) {
+  return jokes[Math.floor(Math.random() * jokes.length)];
+}
+
+// Function to save joke to a jokes.txt
+async function saveJokeToFile(joke) {
+  try {
+    await fs.appendFile("jokes.txt", `${joke.joke}\n\n`);
+    console.log("Joke saved to jokes.txt for future laughs!");
+  } catch (err) {
+    console.error("Error saving joke to file:", err);
+  }
+}
+
+
+if (command === "searchTerm" && searchTerm) {
+  getJokes(searchTerm);
 } else {
-  console.error('Invalid command. Please use "searchTerm" ');
+  console.error(
+    "Please provide a valid command and search term. Usage: node script.js searchTerm <your_search_term>"
+  );
 }
